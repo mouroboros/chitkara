9924149e6220713ea386cbbb42fb55b1760cd568
mgl <m.g.lee@cs.bham.ac.uk>
2017-09-27 12:59:45 +0100

    corrected url for local host to 8000

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,5 @@
 
 from selenium import webdriver
 browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
-browser.get('http://localhost:800')
+browser.get('http://localhost:8000')
 assert 'Django' in browser.title
