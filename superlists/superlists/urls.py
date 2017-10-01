d6a84f960bccb29ca72ede722446991731abc5cc
mgl <m.g.lee@cs.bham.ac.uk>
2017-10-01 14:47:06 +0100

    adding / to urls.py

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from lists import views
+
+# I've removed admin because we won't be using it. You can keep it if you wish
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home'),
 ]
